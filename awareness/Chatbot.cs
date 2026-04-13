01ef972072ae823eb17e418d0f80cedfead86a4c
Student <Student@LabVM2049939>
2026-04-13 14:30:01 +0200

    Add project files.

---
+++ awareness/Chatbot.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Threading.Tasks;
+
+namespace awareness
+{
+    internal class Chatbot
+    {
+    
+            private readonly string asciiBot = @"
+ ________________
+|CYBER AWARENESS |
+| _______________|
+";
+
+            public void PlayWelcomeSound()
+            {
+                try
+                {
+                    SoundPlayer player = new SoundPlayer(awareness.Properties.Resources.voice);
+                    player.Play();
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"(Audio failed to play: {ex.Message})");
+                }
+            }
+
+            public void GreetUser()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("CYBERSECURITY AWARENESS");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("------------------------");
+                Console.ResetColor();
+            }
+
+            public string GetUserName()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write("What is your name? : ");
+                Console.ResetColor();
+                return Console.ReadLine();
+            }
+
+            public void ShowAsciiBot()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine(asciiBot);
+                Console.ResetColor();
+            }
+
+            public void ShowWelcomeMessage(string name)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine($"Hello {name} WELCOME TO THE CYBERSECURITY AWARENESS BOT!", 20);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("How are you?: ");
+                Console.ResetColor();
+                Console.ReadLine(); // Just for interaction
+            }
+
+            public void DisplayInvalidOptionMessage()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("I didn't quite understand that. Please rephrase.", 20);
+                Console.ResetColor();
+            }
+
+            public void DisplayGoodbyeMessage()
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Goodbye", 20);
+                Console.ResetColor();
+            }
+
+            public void TypeText(string text, int delay)
+            {
+                foreach (char c in text)
+                {
+                    Console.Write(c);
+                    Thread.Sleep(delay);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+
---
+++ awareness/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using awareness;
+using AwarenessBot;
+using System;
+
+
+namespace CybersecurityAwareness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chatbot bot = new Chatbot();
+            Menu menu = new Menu();
+
+            bot.PlayWelcomeSound();
+            bot.GreetUser();
+
+            string name = bot.GetUserName();
+            bot.ShowAsciiBot();
+            bot.ShowWelcomeMessage(name);
+
+            bool continueRunning = true;
+
+            while (continueRunning)
+            {
+                string purpose = menu.ShowMainMenu();
+
+                if (purpose == "4")
+                {
+                    bot.DisplayGoodbyeMessage();
+                    continueRunning = false;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
+ 
